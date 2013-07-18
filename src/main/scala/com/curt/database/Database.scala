ff369e7c1ba6617c76b503e6cb529140a7dc3d32
Alex Ninneman <ninnemana@gmail.com>
2013-07-18 01:58:09 -0500

    adding database configuration back in without sensitive data

---
+++ src/main/scala/com/curt/database/Database.scala
@@ -0,0 +1,25 @@
+package com.curt.database
+
+import com.mchange.v2.c3p0.ComboPooledDataSource
+import org.squeryl.{Session, SessionFactory,Schema, KeyedEntity, Query}
+import org.squeryl.adapters.MSSQLServer
+import com.curt.vehicle._
+
+trait Database {
+  val dbUsername = ""
+	val dbPassword = ""
+	var dbConnection = "jdbc:sqlserver://;database="
+
+	var cpds = new ComboPooledDataSource
+	cpds.setDriverClass("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+	cpds.setJdbcUrl(dbConnection)
+	cpds.setUser(dbUsername)
+	cpds.setPassword(dbPassword)
+
+	cpds.setMinPoolSize(1)
+	cpds.setAcquireIncrement(1)
+	cpds.setMaxPoolSize(100)
+
+	SessionFactory.concreteFactory = Some(() => Session.create(cpds.getConnection, new MSSQLServer))
+
+}
